ea37e0ecbd02994617c84dc2b57ac36d7c4f4bc2
McRonald-S <71404620+McRonald-S@users.noreply.github.com>
2022-11-17 14:53:34 -0800

    Update boot.py

--- boot.py
+++ boot.py
@@ -1,1 +1,34 @@
 #Informacion
+ ## MATERIA: SISTEMAS PROGRAMABLES
+  
+´´´python
+try:
+  import usocket as socket
+except:
+  import socket
+
+from machine import Pin
+import network
+
+
+import gc
+gc.collect()
+
+ssid = 'Velasques'
+password = '1234567891'
+
+station = network.WLAN(network.STA_IF)
+
+station.active(True)
+station.connect(ssid, password)
+
+while station.isconnected() == False:
+  pass
+
+print('Connection successful')
+print(station.ifconfig())
+
+led = Pin("LED", Pin.OUT)
+led.on()
+
+´´´
